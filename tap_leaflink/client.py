fc83248c5909bc8cefb410d47ceea8bf311e0515
Philip Johnson <johnson.philip.louis@gmail.com>
2022-08-25 11:56:37 -0500

    roll back a bit

--- tap_leaflink/client.py
+++ tap_leaflink/client.py
@@ -71,10 +71,10 @@ class leaflinkStream(RESTStream):
                 params = dict(parse.parse_qsl(parse.urlsplit(next_page_token).query))
                 offset = int(params.get('offset', 0))
                 print(str(round((offset / count) * 100, 2)) + "%")
+                
         else:
             next_page_token = response.headers.get("X-Next-Page", None)
         
-            return params
         return next_page_token
 
     def get_url_params(
@@ -90,14 +90,14 @@ class leaflinkStream(RESTStream):
             params = dict(parse.parse_qsl(parse.urlsplit(next_page_token).query))
 
             # State management
-            context_state = self.get_context_state(context)
-            last_updated = context_state.get("replication_key_value")
+            # context_state = self.get_context_state(context)
+            # last_updated = context_state.get("replication_key_value")
 
-            start_date = self.config.get("start_date")
+            # start_date = self.config.get("start_date")
 
-            if last_updated:
-                params["modified__gt"] = last_updated
-                return params
+            # if last_updated:
+            #     params["modified__gt"] = last_updated
+            #     return params
 
             # elif start_date:
             #     params["created_on__gt"] = start_date
